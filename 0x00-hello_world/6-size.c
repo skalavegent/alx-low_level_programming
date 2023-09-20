eab71d8f35466df80d920c020d00119b079a8d50
skalavegent <kabelomoremi67@gmail.com>
2023-09-20 23:23:52 +0000

    6.size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of int: %lu byte(s)", sizeof(int));
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("SIze of float: %lu byte(s)", sizeof(float));
+	printf("Size of float: %lu byte(s)", sizeof(float));
 	return (0);
 }
