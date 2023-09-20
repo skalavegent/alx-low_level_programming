818a640aec81ddb4256ce308ea7c3d0f6f3242bb
skalavegent <kabelomoremi67@gmail.com>
2023-09-20 22:37:15 +0000

    4.puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - this is the main function
  i*
- * return: always 0 (Success)
+ * return: always 0
  */
 
-int main (void)
+int main(void)
 {
-	puts("\""Programming is like building  a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
