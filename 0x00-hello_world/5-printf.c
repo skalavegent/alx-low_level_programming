a80cba7b136e9c793e20042b0672948250bf4d87
skalavegent <kabelomoremi67@gmail.com>
2023-09-20 22:58:18 +0000

    5.printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: 0 if succesful
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
