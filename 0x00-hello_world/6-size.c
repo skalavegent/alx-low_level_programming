093d24e248d75ebe5c0b14caa8927dca0c3c8496
skalavegent <kabelomoremi67@gmail.com>
2023-09-21 05:56:16 +0000

    6.size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,8 +16,8 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
-	printf("Size of float: %lu byte(s)", (unsigned long)sizeof(d));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
