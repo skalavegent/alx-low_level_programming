aedd27533b4dae9f7d58967e4dde1e42f544323f
skalavegent <kabelomoremi67@gmail.com>
2023-09-22 11:12:41 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,13 +8,14 @@
  * @n: input value
  * Return:dest
  */
-char *_strncat(char *dest, char *src, int n);
+
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
 	i = 0;
-	while (dest[i] != '/0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
