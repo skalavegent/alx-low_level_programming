7b09810c5984394bb40234550d44c2535edcd280
vagrant <vagrant@vagrant.vm>
2023-10-17 16:48:18 +0000

    98 battery st

--- 0x05-pointers_arrays_strings/main_0.c
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -15,21 +15,4 @@ int main(void)
 		        reset_to_98(&n);
 			    printf("n=%d\n", n);
 			        return (0);
-}#include "main.h"
-#include <stdio.h>
-
-/**
- *  * main - check the code 
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-	    int n;
-
-	        n = 402;
-		    printf("n=%d\n", n);
-		        reset_to_98(&n);
-			    printf("n=%d\n", n);
-			        return (0);
 }
